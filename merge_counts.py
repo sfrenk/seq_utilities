3351be545b1e1f122d65b34802ec1729e75f71b9
sfrenk <stephen.frenk@gmail.com>
2016-10-06 21:19:59 -0400

    rewrote merge_counts.py

--- merge_counts.py
+++ merge_counts.py
@@ -10,33 +10,37 @@ import argparse
 
 # Get target directory
 
-parser = argparse.ArgumentParser(description="Merge multiple count files into a single count table")
+parser = argparse.ArgumentParser(description = "Merge multiple count files into a single count table")
 
-parser.add_argument("directory", help="directory containing count files (each file must end with '.txt'")
+parser.add_argument("directory", help = "directory containing count files (each file must end with '.txt'")
+parser.add_argument("-o", "--output", help = "output file name (default: count_table.txt", default = "count_table.txt")
 
 args = parser.parse_args()
-
-directory = args.directory 
-os.chdir(directory)
 
 # Merge files
 
 count_files = []
 
-for f in os.listdir("."):
-	if f.endswith("counts.txt"):
+for f in os.listdir(args.directory):
+	if f.endswith("_counts.txt"):
 		count_files.append(f)
 
-merged = pd.read_csv(count_files[0], sep="\s", header=None, names=[count_files[0], 'gene'])
-merged = merged[['gene', count_files[0]]]
+# Read in first count file. All further counts will be appended to this data frame
 
-for i in range(1, len(count_files)):
-	count_file = pd.read_csv(count_files[i], sep="\s", header=None, names=[count_files[i], 'gene'])
+merged = pd.read_csv(count_files[0], sep="\s", header=None, names=[count_files[0].replace("_counts.txt", ""), 'gene'])
+merged = merged[['gene', count_files[0].replace("_counts.txt", "")]]
+
+# Remove the first count file from the list so that it doesn't get counted twice
+
+count_files = count_files[1:]
+
+for i in count_files:
+	count_file = pd.read_csv(i, sep="\s", header=None, names=[i.replace("_counts.txt", ""), 'gene'])
 	merged = merged.merge(count_file, how='left', left_on='gene', right_on='gene')
 
 # Get rid of the row containing unmapped reads
 
-merged = merged[merged['gene'] != '*']
+merged = merged[merged.gene != '*']
 
 # If a particular transcript was detected in some samples but not others, the samples in which the transcript was absent will report an "NA" value after merging of the count files. This needs to be changed to "0" for downstream analysis
 
@@ -44,5 +48,4 @@ merged = merged.fillna(0)
 
 # Save the count table
 
-merged.to_csv('count_table.txt', sep='\t', index=False)
-
+merged.to_csv(args.output, sep='\t', index=False)
