8136ee1c544fa60af2da0ed4fe2c589689935a63
sfrenk <stephen.frenk@gmail.com>
2016-10-17 11:17:24 -0400

    New version - now has increased filtering flexibility

--- small_rna_filter.py
+++ small_rna_filter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 ###############################################################################
-# Extract all 22G sRNAs fastq, fasta or raw file. This gives a .txt file containing raw sequences, which can be mapped in bowtie using the -r flag
+# small_rna_filter.py version 2.0
+#
+#
+# Filter reads from a fastq, fasta or raw file. Output is a .txt file containing raw sequences, which can be mapped in bowtie using the -r flag
 ###############################################################################
 
 import os
@@ -10,39 +13,39 @@ import gzip
 
 parser = argparse.ArgumentParser(description="Extract 22g and/or 21u rnas from fastq, fasta or raw text file")
 
-parser.add_argument("I", help = "Input file. File type will be detected based on the first line of the file. File may be in gzipped format.")
-parser.add_argument("-o", help = "Output file.", required = True)
-parser.add_argument("-g", help = "Retrieve 22g RNAs.", action = "store_true")
-parser.add_argument("-u", help = "Retrieve 21u RNAs.", action = "store_true")
-parser.add_argument("-s", help = "If not filtering for 22g or 21u RNAs, you can specify size range of reads to keep by providing min and max length seperated by a comma (eg. to keep reads between 19 and 24 nucleotides (inclusive), use '-s 19,24' (Default size range: 18 to 30 nucleotides)", default = "18,30")
+parser.add_argument("infile", help = "Input file. File type will be detected based on the first line of the file. File may be in gzipped format.")
+parser.add_argument("-o", "--output", help = "Output file.", required = True)
+parser.add_argument("-f", "--filter", help = "Filter reads based on the first nucleotide (eg. to select 22g RNAs, use options -f g -s 22,22", default = "A,T,G,C")
+parser.add_argument("-s", "--size", help = "Specify size range of reads to keep by providing min and max length seperated by a comma (eg. to keep reads between 19 and 24 nucleotides (inclusive), use '-s 19,24' (Default size range: 18 to 30 nucleotides)", default = "18,30")
 
 args = parser.parse_args()
 
-infile = args.I
-outfile = args.o
+# Define starting nucleotide
+args.filter = args.filter.upper()
+first_base = args.filter.split(",")
 
 # Define minimum and maximum read size
 
-min_size = int(args.s.split(",")[0])
-max_size = int(args.s.split(",")[1])
+min_size = int(args.size.split(",")[0])
+max_size = int(args.size.split(",")[1])
 
 ###############################################################################
 
-print '##### small_rna_filter.py #####'
+print("##### small_rna_filter.py #####")
 
 # Input file may or may not be gzipped
 
-if infile.endswith(".gz"):
-	f = gzip.open(infile, "rb")
+if args.infile.endswith(".gz"):
+	f = gzip.open(args.infile, "rb")
 else:
-	f = open(infile, "r")
+	f = open(args.infile, "r")
 
 seqs = f.readlines()
 
 reads = []
 
 if seqs[0].startswith("@"):
-	print 'extracting sequence information from fastq file'
+	print("extracting sequence information from fastq file")
 	# Each fastq record consists of four lines. The sequence info is on the second line.
 	for i, line in enumerate(seqs):
 		if i % 4 == 1:
@@ -50,11 +53,11 @@ if seqs[0].startswith("@"):
 	reads = [line.strip() for line in reads]
 
 elif seqs[0].startswith(">"):
-	print 'extracting sequence information from fasta file'
+	print("extracting sequence information from fasta file")
 	reads = [line.strip() for line in seqs if line[0] != '>']
 
 elif seqs[0][0].isalpha():
-	print 'extracting sequence information from raw text file'
+	print("extracting sequence information from raw text file")
 	reads = [line.strip() for line in seqs]
 
 else:
@@ -62,47 +65,22 @@ else:
 
 f.close()
 
-print 'input reads:'
-print(len(reads))
+print("input reads: " + str(len(reads)))
 
-G22 = []
-U21 = []
+# Extract reads based on filtering parameters
 
-# Extract 22G RNAs
+print("extracting reads in size range "+ str(min_size) + " to " + str(max_size) + " nucleotides with " + args.filter + " as the first nucleotide")
 
-if args.g:
-	G22 = [x for x in reads if len(x) == 22 and x[0] == 'G']
-	print '22G RNAs extracted:'
-	print(len(G22))
+reads = [x for x in reads if len(x) >= min_size and len(x) <= max_size and x[0].upper() in first_base]
 
-# Extract 21U RNAs
-
-if args.u:
-	U21 = [x for x in reads if len(x) == 21 and x[0] == 'T']
-	print '21U RNAs extracted:'
-	print(len(U21))
-
-# Combine all filtered reads
-
-reads_out = G22 + U21
-
-# If neither -g nor -u were specified, just convert input reads to raw format
-
-if args.g == False and args.u == False:
-	print('no sRNA type selected. Selecting for reads that are ' + str(min_size) + ' to ' + str(max_size) + ' nucleotides in length and converting all reads to raw format')
-	reads_out = reads
-
-	# Select reads that fall in the expected size range (18-30 nucleotides)
-
-	reads_out = [x for x in reads_out if len(x) >= min_size and len(x) <= max_size]
+print("output reads: " + str(len(reads)))
 
 # Write out all (filtered) reads to raw txt file
 
-print 'writing filtered sequences to:'
-print(outfile)
+print('writing filtered sequences to: ' + args.output)
 
-with open(outfile, 'w') as f:
-	for line in reads_out:
+with open(args.output, 'w') as f:
+	for line in reads:
 		f.write(line+'\n')
 
-print('#####	Finished	#####')
+print("#####	Finished	#####")
