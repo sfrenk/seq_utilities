a4b8f29a996904dfd81ceb06ca232f27beb05444
sfrenk <stephen.frenk@gmail.com>
2018-10-15 16:03:29 -0400

    Made ID conversion step optional

--- go_terms.R
+++ go_terms.R
@@ -9,23 +9,36 @@ parser <- ArgumentParser(description = "Find GO terms for a list of genes")
 
 parser$add_argument("file", help = "Input file (list of genes, one gene per line)")
 parser$add_argument("-o", "--output", help = "output filename (default: go_terms.txt))", default = "go_terms.txt")
+parser$add_argument("-c", "--convert", help = "Use this option to convert gene IDs from gene name to wormbase ID", default = FALSE, action = "store_true")
 
 args <- parser$parse_args()
 
 # Load data
 genes <- read.table(args$file)$V1
 
-# Set up bioMart
-# There's sometimes a problem with connecting to biomart, so need to keep trying if there is an error
-try_counter <- 0
-while(!exists("mart") & try_counter < 10){
-    tryCatch(mart <- useMart(biomart="ensembl", dataset="celegans_gene_ensembl"), error = function(x) {Sys.sleep(10)})
-    try_counter <- try_counter + 1
-}
-
-if (!exists("mart")){
-    print("ERROR: Could not connect to Biomart")
-    quit(save = "no", status = 1, runLast = FALSE)
+# Convert gene IDs if necessary
+if (args$convert){
+    # Set up bioMart
+    # There's sometimes a problem with connecting to biomart, so need to keep trying if there is an error
+    try_counter <- 0
+    while(!exists("mart") & try_counter < 10){
+        tryCatch(mart <- useMart(biomart="ensembl", dataset="celegans_gene_ensembl"), error = function(x) {Sys.sleep(10)})
+        try_counter <- try_counter + 1
+    }
+    
+    if (!exists("mart")){
+        print("ERROR: Could not connect to Biomart")
+        quit(save = "no", status = 1, runLast = FALSE)
+    }
+    
+    # Convert gene names to Wormbase IDs for DAVID
+    results <- getBM(attributes = "wormbase_gene", 
+                     filters = "external_gene_name", 
+                     values = genes,
+                     mart = mart)
+    gene_list <- results$wormbase_gene
+} else{
+    gene_list <- genes
 }
 
 print("Finding GO terms..")
@@ -33,16 +46,10 @@ print("Finding GO terms..")
 # Connect to DAVID web service
 david <- DAVIDWebService$new(email="sfrenk@unc.edu", url="https://david.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
 
-# Convert gene names to Wormbase IDs for DAVID
-results <- getBM(attributes = "wormbase_gene", 
-                 filters = "external_gene_name", 
-                 values = genes,
-                 mart = mart)
-
 # Define gene list for DAVID
-result <- addList(david, results$wormbase_gene, idType = "WORMBASE_GENE_ID", listName = "list", listType = "Gene")
+go_result <- addList(david, gene_list, idType = "WORMBASE_GENE_ID", listName = "list", listType = "Gene")
 # See proportion of genes mapped by david
-print(paste0("Proportion of genes recognized by DAVID: ", result))
+print(paste0("Proportion of genes recognized by DAVID: ", go_result[[1]]))
 
 # Specify annotation categories
 setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
